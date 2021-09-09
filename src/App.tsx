11afea1db1cf54c93000f6dc69ef10ef29a0534f
leekyungwoo <thanisorn_c@cmu.ac.th>
2021-09-09 14:27:08 +0700

    this is my laugh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { comments, CommentType } from './data/comments';
 
 function App() {
+  function commentMap() {
+    const com = comments.map(x => {
+      return (
+        <div>
+          {/* comments section */}
+          <div className="">
+            {/* normal comment */}
+            <div className="flex p-2 items-start space-x-2">
+              <img className="w-10 w-10 rounded-full" src={x.userImagePath}></img>
+              <div className="bg-gray-200 rounded-lg p-2">
+                <p className="font-semibold">{x.username}</p>
+                <p>{x.commentText}</p>
+                {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
+                {x.likeNum > 0 ?
+                  <div className='flex items-center'>
+                    <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                    <p className='text-gray-500'>{x.likeNum} คน</p>
+                  </div> : null
+                }
+
+              </div>
+            </div>
+            {/* replies */}
+            {/* ต่างกันตรงที่มี padding มากกว่าเท่านั้น (pl-14) */}
+            {repMap(x.replies)}
+
+
+          </div>
+        </div>
+      )
+    })
+    return com
+  }
+
+  function repMap(x: CommentType[]) {
+    const rep = x.map(t => {
+      return (
+        <div>
+          <div className="flex p-2 items-start space-x-2 pl-14">
+            <img className="w-10 w-10 rounded-full" src={t.userImagePath}></img>
+            <div className="bg-gray-200 rounded-lg p-2">
+              <p className="font-semibold">{t.username}</p>
+              <p>{t.commentText}</p>
+              {t.likeNum > 0 ?
+                <div className='flex items-center'>
+                  <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                  <p className='text-gray-500'>{t.likeNum} คน</p>
+                </div> : null
+              }
+            </div>
+          </div>
+
+        </div>
+      )
+    })
+    return rep
+  }
   return (
     <div className="p-2">
       {/* post container */}
@@ -13,8 +71,8 @@ function App() {
 
           {/* image and name */}
           <div className="flex space-x-2 items-center">
-            <img className="w-12 h-12 rounded-full" src="/profileImages/handsome.jpg"></img>
-            <span className='font-semibold text-lg'>Chayanin Suatap 610631100</span>
+            <img className="w-12 h-12 rounded-full" src="/profileImages/pic.png"></img>
+            <span className='font-semibold text-lg'>Thanisorn Chaiwut 630610738</span>
           </div>
 
           {/* status message */}
@@ -27,36 +85,7 @@ function App() {
           </div>
 
         </div>
-
-        {/* comments section */}
-        <div className="">
-
-          {/* normal comment */}
-          <div className="flex p-2 items-start space-x-2">
-            <img className="w-10 w-10 rounded-full" src="/profileImages/lisa.jpg"></img>
-            <div className="bg-gray-200 rounded-lg p-2">
-              <p className="font-semibold">Lisa</p>
-              <p>ตัวอย่าง Template จ้า</p>
-              {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
-              <div className='flex items-center'>
-                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
-                <p className='text-gray-500'>999 คน</p>
-              </div>
-            </div>
-          </div>
-
-          {/* replies */}
-          {/* ต่างกันตรงที่มี padding มากกว่าเท่านั้น (pl-14) */}
-          <div className="flex p-2 items-start space-x-2 pl-14">
-            <img className="w-10 w-10 rounded-full" src="/profileImages/puppy.jpg"></img>
-            <div className="bg-gray-200 rounded-lg p-2">
-              <p className="font-semibold">หมาน้อย</p>
-              <p>เม้นค้าบ</p>
-            </div>
-          </div>
-
-        </div>
-
+        {commentMap()}
       </div>
     </div>
   );
